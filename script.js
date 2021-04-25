ab0f4a649f6eb657d55f972a28b04ca0b552e561
Margarita-Pepperoni <77731018+Margarita-Pepperoni@users.noreply.github.com>
2021-04-25 16:52:26 +0300

    ex.2-Calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+function calcSum()
+{
+let a = prompt('введите слагаемое 1');
+let b = prompt('введите слагаемое 2');
+let calcSum = (+a + +b);
+alert (calcSum);
+}
+
+function calcSubtract()
+{
+let a = prompt('введите уменьшаемое');
+let b = prompt('введите вычитаемое');
+let calcSubtract = (+a - +b);
+alert (calcSubtract);
+}
+
+function calcMultipl()
+{
+let a = prompt('введите множитель 1');
+let b = prompt('введите множитель 2');
+let calcMultipl = (+a * +b);
+alert (calcMultipl);
+}
+
+function calcDivid()
+{
+let a = prompt('введите делимое');
+let b = prompt('введите делитель');
+let calcDivid = (+a / +b);
+alert (calcDivid);
+}
+
+
